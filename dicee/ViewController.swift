2e6d79ca84eea1274c6ee50972e029ef4fbc154a
Lucy Wang <wang796@usc.edu>
2018-06-23 14:30:32 -0500

    UI completed

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var randomDiceIndex1 : Int = 0;
+    var randomDiceIndex2 : Int = 0;
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // random number generator 
+    }
+    
 }
 
